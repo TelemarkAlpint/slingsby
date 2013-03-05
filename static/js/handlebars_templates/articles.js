0f29a58920e06e835b269531ce1b0412b50b4779
Tarjei Husøy <tarjei@roms.no>
2013-03-06 00:16:03 +0100

    Updated to the latest compiled template.

--- static/js/handlebars_templates/articles.js
+++ static/js/handlebars_templates/articles.js
@@ -2,7 +2,7 @@
 
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 
-templates['article'] = template(function (Handlebars,depth0,helpers,partials,data) {
+templates['articles'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
   var stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
