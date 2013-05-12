2ac71905d2d4a2e35fa403755c83f6ec5f63a014
Tarjei Husøy <tarjei@roms.no>
2013-05-12 13:36:27 +0200

    Removed some legacy code and improved readability of main script.

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,51 @@
-$(".upload").click(function () {
-    $(".uploadform").toggle("slide");
-});
+(function(window, document, undefined){
+	$("#suggest_quote").click(function () {
+	    $("#quote_suggestion").slideToggle(300);
+	    $(this).html(($(this).text() == "+") ? "-" : "+");
+	});
 
-$("#uploadq").click(function () {
-    $("#quoteform").toggle("slide");
-});
+	$('.quote_reject').submit(function(){
+		var target = $(this).attr('action');
+		$.ajax(target, {
+			type: "POST",
+			context: $(this),
+			success: function(){
+				var wrapper = $(this).parents('.suggested_quote_wrapper');
+				wrapper.slideUp(function(){
+					wrapper.remove();
+				});
+			},
+		});
+		return false;
+	});
 
-$("#suggest_quote").click(function () {
-    $("#quote_suggestion").slideToggle(300);
-    $(this).html(($(this).text() == "+") ? "-" : "+");
-});
+	$('.quote_confirm').submit(function(){
+		var target = $(this).attr('action');
+		$.ajax(target, {
+			type: "POST",
+			context: $(this),
+			success: function(){
+				$('.suggested_quote_wrapper').slideUp(function(){
+					$(this).remove();
+				});
+			},
+		});
+		return false;
+	});
 
-function toggle_submenu(){
-    $("#submenu").slideToggle(200);
-}
-
-function toggle_submenu2(){
-    $("#submenu2").slideToggle(200);
-}
-
-Modernizr.addTest('meter', function(){
-    var elem = document.createElement('meter');
-    return !(elem instanceof HTMLUnknownElement);
-});
+	// Add click handling for the statusbar in the header
+    $('#status .clickable').click(function(){
+        var clickSymbol = '&#9660;' // BLACK DOWN POINTING TRIANGLE
+        if ($(this).hasClass('selected')){
+            $(this).removeClass('selected');
+            clickSymbol =  '&#9664;' // BLACK LEFT POINTING TRIANGLE
+            $(this).parent().find('ul').slideUp();
+            $(this).find('.click_symbol').removeClass('rotated_left');
+        } else {
+            $(this).addClass('selected');
+            $(this).parent().find('ul').slideDown();
+            $(this).find('.click_symbol').addClass('rotated_left');
+        }
+        //$(this).find('.click_symbol').html(clickSymbol);
+    });
+})(this, document);
