9302229ce039344626c9b651937c7d6a48862b49
Tarjei Husøy <tarjei@comoyo.com>
2013-06-22 19:21:17 +0200

    Delint breakout script.

--- static-src/js/breakout.js
+++ static-src/js/breakout.js
@@ -1,6 +1,8 @@
-function breakout_of_frame(){
-    var request_data = document.location.search;
+(function breakoutOfFrame() {
+    "use strict";
+
+    var requestData = document.location.search;
     var subpage = document.location.pathname.substr(18);
-    var redirect_url = 'http://www.ntnuita.no';
-    top.location.href = redirect_url + subpage + request_data;
-}
+    var redirectUrl = 'http://www.ntnuita.no';
+    top.location.href = redirectUrl + subpage + requestData;
+}());
