9e60b99335c140f05c3849892da085e8ea4128c8
Tarjei Husøy <tarjei@comoyo.com>
2013-06-22 19:06:07 +0200

    Store users in cache.

--- general/middleware.py
+++ general/middleware.py
@@ -1,3 +1,7 @@
+from django.contrib import auth
+from django.utils.functional import SimpleLazyObject
+from general import cache
+
 class HttpAcceptMiddleware(object):
     """ Parse the HTTP_ACCEPT header and add it to the request object.
 
@@ -31,3 +35,23 @@ class HttpAcceptMiddleware(object):
         request.http_accept = accepts
         request.prefer_html = html_priority > json_priority
         request.prefer_json = json_priority > html_priority
+
+
+class CachedAuthMiddleware(object):
+    """ Store session user in cache, to avoid hitting the database on every pageview. """
+
+    @staticmethod
+    def get_user(request):
+        user_id = request.session.get('_auth_user_id')
+        if user_id is None:
+            user = auth.get_user(request)
+        else:
+            user = cache.get('auth_user-%d' % user_id)
+            if user is None:
+                user = auth.get_user(request)
+                cache.set('auth_user-%d' % user_id, user)
+        return user
+
+    def process_request(self, request):
+        assert hasattr(request, 'session'), "The CachedAuthMiddleware requires the django.contrib.sessions.middleware.SessionMiddleware to be installed!"
+        request.user = SimpleLazyObject(lambda: CachedAuthMiddleware.get_user(request))
