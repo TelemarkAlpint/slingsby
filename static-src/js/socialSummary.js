57f40ebf95a5c063a1a48a4b053b451ac0f607eb
Tarjei Husøy <tarjei@comoyo.com>
2013-06-22 19:23:40 +0200

    Delint socialSummary.js

--- static-src/js/socialSummary.js
+++ static-src/js/socialSummary.js
@@ -1,11 +1,11 @@
-$(function(){
+(function ($) {
     "use strict";
 
-    $(".social_summary_textbox").each(function(){
+    $(".social_summary_textbox").each(function () {
         $(this).after('<br><span>Antall tegn: 0</span>');
-            $(this).keyup(function(){
-                var length = this.value.length;
-                $(this).siblings("span").html("Antall tegn: " + length);
-            });
+        $(this).keyup(function () {
+            var length = this.value.length;
+            $(this).siblings("span").html("Antall tegn: " + length);
+        });
     });
-});
+}(jQuery));
