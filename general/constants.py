586a90cc979cc0a54f41bd6d600b096ffe58cc49
Tarjei Husøy <tarjei@comoyo.com>
2013-07-15 16:59:28 +0200

    Fix ntnui uris to use their old pages.

--- general/constants.py
+++ general/constants.py
@@ -1,9 +1,9 @@
 
-LOGIN_URL = 'http://ntnui.no/authapi/telemark'
+LOGIN_URL = 'http://old.ntnui.no/authapi/telemark'
 
-LOGOUT_URL = 'http://ntnui.no/Logout.xhtml'
+LOGOUT_URL = 'http://old.ntnui.no/Logout.xhtml'
 
-LOGIN_REDIRECT = 'http://ntnui.no/telemark'
+LOGIN_REDIRECT = 'http://old.ntnui.no/telemark'
 
 MEDIA_DIR = 'http://org.ntnu.no/telemark/media/'
 
@@ -13,10 +13,10 @@ JSON_ARCHIVE_PATH = 'http://org.ntnu.no/telemark/arkiv/arrangement/archive.json'
 
 DEFAULT_TITLE = 'NTNUI Telemark/Alpint'
 
-JOIN_URL = 'http://ntnui.no/telemark/Join.xhtml'
+JOIN_URL = 'http://old.ntnui.no/telemark/Join.xhtml'
 
-LEAVE_URL = 'http://ntnui.no/telemark/Leave.xhtml'
+LEAVE_URL = 'http://old.ntnui.no/telemark/Leave.xhtml'
 
-EDIT_PROFILE_IMAGE = 'http://www.ntnui.no/Profile/Edit/Image.xhtml'
+EDIT_PROFILE_IMAGE = 'http://old.ntnui.no/Profile/Edit/Image.xhtml'
 
 ARCHIVE_BASE_PATH = 'http://org.ntnu.no/telemark/arkiv/arrangement/'
