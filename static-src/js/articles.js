1bc82859473618e4e5ae71feeffa168510ab2a8a
Tarjei Husøy <tarjei@comoyo.com>
2013-07-27 17:05:33 +0200

    Add articles.js to static-src

---
+++ static-src/js/articles.js
@@ -0,0 +1,10 @@
+moment.lang("nb");
+function improveTimestamps(){
+    $('.isotimestamp').each(function(){
+        var timestamp = $(this).attr('data-timestamp');
+        var m = moment(timestamp);
+        $(this).html(m.calendar());
+        $(this).css("opacity", 1);
+        $(this).attr("title", m.format("LLL"));
+    });
+};  
