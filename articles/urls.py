f84f64f9807f212e75808925a3a178771a68f819
Tarjei Husøy <tarjei@comoyo.com>
2013-07-27 16:55:06 +0200

    Use class-based views for articles.

--- articles/urls.py
+++ articles/urls.py
@@ -1,16 +1,16 @@
-from articles.views import SingleArticlePageQuery
+from articles.views import SingleArticlePageQuery, AllArticlesList, LatestArticlesList, ArticleDetail
 from django.conf.urls import patterns, url
 from django.core.urlresolvers import reverse
 from django.http import HttpResponsePermanentRedirect
 
 urlpatterns = patterns('articles.views',
-    url(r'^$', 'latest_articles', name='frontpage'),
-    url(r'^artikkel/(\d+)/$', lambda req, pk: HttpResponsePermanentRedirect(reverse('article_detail', args=[str(pk)]))), #Deprecated!
-    url(r'^articles/(\d+)/$', 'show_article', name='article_detail'),
-    url(r'^articles/$', 'all_articles', name='all_articles'),
+    url(r'^$', LatestArticlesList.as_view(), name='frontpage'),
+    url(r'^artikkel/(\d+)/$', lambda req, pk: HttpResponsePermanentRedirect(reverse(ArticleDetail.as_view(), args=[str(pk)]))), #Deprecated!
+    url(r'^articles/(\d+)/$', ArticleDetail.as_view(), name='article_detail'),
+    url(r'^articles/$', AllArticlesList.as_view(), name='all_articles'),
                         )
 
 urls = []
 for subpage in SingleArticlePageQuery.get_cached():
-    urls.append(url(r'^%s/$' % subpage['slug'], 'articles.views.show_article', {'article_id': subpage['id']}))
+    urls.append(url(r'^%s/$' % subpage.slug, ArticleDetail.as_view(), {'article_id': subpage.id}))
 urlpatterns += patterns('', *urls)
--- articles/views.py
+++ articles/views.py
@@ -4,6 +4,8 @@ from django.db.models.signals import post_save, post_delete
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404
 from django.views.generic.simple import direct_to_template
+from django.views.generic.base import View
+from general import make_title
 from general.cache import CachedQuery
 from general.time import aware_from_utc
 import json
@@ -12,57 +14,72 @@ import logging
 logger = logging.getLogger(__name__)
 
 class SingleArticlePageQuery(CachedQuery):
-    queryset = SubPageArticle.objects.all().values('slug', 'title', 'visible', 'id')
+    queryset = SubPageArticle.objects.all()
 post_save.connect(SingleArticlePageQuery.empty_on_save, sender=SubPageArticle)
 post_delete.connect(SingleArticlePageQuery.empty_on_save, sender=SubPageArticle)
 
+
 class LatestArticlesQuery(CachedQuery):
-    queryset = Article.objects.all().select_related().values('visible', 'content', 'title', 'id', 'published_date', 'last_edited', 'last_edited_by__username', 'author__username')[:5]
+    queryset = Article.objects.all().select_related()[:5]
 post_save.connect(LatestArticlesQuery.empty_on_save, sender=Article)
 
+
 class AllArticlesQuery(CachedQuery):
-    queryset = Article.objects.all()
+    queryset = Article.objects.all().select_related()
 post_save.connect(AllArticlesQuery.empty_on_save, sender=Article)
 
-def latest_articles(request):
-    values = {
-              'articles': LatestArticlesQuery.get_cached(),
-              }
-    return direct_to_template(request, 'articles/article_list.html', values)
 
-def _get_filtered_articles(request):
-    before = request.GET.get('before')
-    if before is None:
-        articles = AllArticlesQuery.get_cached()
-    else:
-        published_date_filter = parse(before)
-        utc_date = aware_from_utc(published_date_filter)
-        articles = Article.objects.filter(published_date__lt=utc_date, visible=True)
-    num_limit = int(request.GET.get('limit', '0'))
-    if num_limit:
-        articles = articles[:num_limit]
-    return articles
+class AllArticlesList(View):
 
-def all_articles(request):
-    articles = _get_filtered_articles(request)
-    if request.prefer_json:
-        json_data = {
-                     'articles': [a.__json__() for a in articles],
-                     }
-        return HttpResponse(json.dumps(json_data), mimetype='application/json')
-    values = {
-              'articles': articles,
-              }
-    return direct_to_template(request, 'articles/article_list.html', values)
+    def _get_filtered_articles(self, request):
+        """ Get all articles, but filter them by the `before` param and limit
+        the number of results to the `limit` param.
+        """
+        before = request.GET.get('before')
+        if before is None:
+            articles = AllArticlesQuery.get_cached()
+        else:
+            published_date_filter = parse(before)
+            utc_date = aware_from_utc(published_date_filter)
+            articles = Article.objects.filter(published_date__lt=utc_date, visible=True)
+        num_limit = int(request.GET.get('limit', '0'))
+        if num_limit:
+            articles = articles[:num_limit]
+        return articles
 
-def show_article(request, article_id):
-    try:
-        article = get_object_or_404(Article, pk=article_id)
-    except Http404:
-        article = get_object_or_404(SubPageArticle, pk=article_id)
-    if request.prefer_json:
-        return HttpResponse(json.dumps(article.__json__()), mimetype='application/json')
-    context = {
-               'article': article,
-               }
-    return direct_to_template(request, 'articles/article_detail.html', context)
+    def get(self, request):
+        articles = self._get_filtered_articles(request)
+        if request.prefer_json:
+            json_data = {
+                'articles': [a.__json__() for a in articles],
+            }
+            return HttpResponse(json.dumps(json_data), mimetype='application/json')
+        values = {
+            'articles': articles,
+        }
+        return direct_to_template(request, 'articles/article_list.html', values)
+
+
+class LatestArticlesList(View):
+
+    def get(self, request):
+        values = {
+            'articles': LatestArticlesQuery.get_cached(),
+        }
+        return direct_to_template(request, 'articles/article_list.html', values)
+
+
+class ArticleDetail(View):
+
+    def get(self, request, article_id):
+        try:
+            article = get_object_or_404(Article, pk=article_id)
+        except Http404:
+            article = get_object_or_404(SubPageArticle, pk=article_id)
+        if request.prefer_json:
+            return HttpResponse(json.dumps(article.__json__()), mimetype='application/json')
+        context = {
+            'article': article,
+            'title': make_title(article.title),
+        }
+        return direct_to_template(request, 'articles/article_detail.html', context)
