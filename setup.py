2c5e20997fb3054d12bccf66f21682f7deae49ca
Tarjei Husøy <tarjei@roms.no>
2013-10-07 18:12:30 +0200

    Add a setup.py, allowing the project to be built properly.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from os import path
+import glob
+
+setup(
+    name='slingsby',
+    version='1.0.0',
+    author='NTNUI Telemark/Alpint',
+    author_email='telemark-webmaster@ntnui.no',
+    url='https://github.com/TelemarkAlpint/slingsby',
+    description='NTNUI Telemark/Alpints website',
+    packages=find_packages(),
+    package_data={
+        '': [
+            path.join('templates', '*.html'),
+            path.join('templates', '*', '*.html'),
+            'log_conf.yaml',
+        ],
+    },
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'manage.py = slingsby:manage'
+        ]
+    }
+)
