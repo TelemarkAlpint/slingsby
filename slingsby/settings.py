6cf4817387d31cdebd1093a2c58324260d88c2de
Tarjei Husøy <tarjei@roms.no>
2013-10-07 18:15:17 +0200

    Split settings into several files.
    This makes it easier to use a custom local settings file, without a lot
    of branching and shit in the main settings.
    
    Also made the dev setup use sqlite3, so that you don't have to install any
    full-blown databases to get started.

--- slingsby/settings.py
+++ slingsby/settings.py
@@ -2,47 +2,24 @@
 
 import os
 
-prod_server = os.environ.get('SERVER_SOFTWARE', '').startswith('Google')
-
-if prod_server:
-    from .secrets import SECRET_KEY, FACEBOOK_API_SECRET
-else:
-    SECRET_KEY = 'pleasedontusethisinprod'
-    FACEBOOK_API_SECRET = 'pleasedontusethisinprod'
-
-DEBUG = not prod_server
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = [
     '.ntnuita.no',
-    '.telemarkalpint.appspot.com',
 ]
-
-if not prod_server:
-    ALLOWED_HOSTS.append('localhost')
 
 USE_TZ = True
 TIME_ZONE = 'Europe/Oslo'
 FIRST_DAY_OF_WEEK = 1 # Søndag = 0, Mandag = 1
 
-if prod_server:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'google.appengine.ext.django.backends.rdbms',
-            'INSTANCE': 'ntnuitelemarkalpint:slingsby-db',
-            'NAME': 'slingsby_rel',
-        },
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'slingsby',
+        'NAME': 'slingsby_rel',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'USER': 'root',
-            'HOST': 'localhost',
-            'NAME': 'dev_db',
-        },
-    }
-AUTOLOAD_SITECONF = 'indexes'
+}
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -54,6 +31,7 @@ INSTALLED_APPS = (
 
     'slingsby.archive',
     'slingsby.articles',
+    'slingsby.auth',
     'slingsby.events',
     'slingsby.gear',
     'slingsby.general',
@@ -61,8 +39,6 @@ INSTALLED_APPS = (
     'slingsby.quotes',
     'slingsby.tasks',
     'slingsby.users',
-
-    'debug_toolbar',
 
     'social_auth',
 )
@@ -72,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
     'slingsby.general.middleware.HttpAcceptMiddleware',
     'slingsby.general.middleware.HttpMethodOverride',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
@@ -82,6 +58,7 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 AUTHENTICATION_BACKENDS = (
   'social_auth.backends.google.GoogleOAuth2Backend',
   'social_auth.backends.facebook.FacebookBackend',
+
   'django.contrib.auth.backends.ModelBackend',
 )
 
@@ -90,16 +67,15 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
     'django.core.context_processors.media',
     'django.core.context_processors.debug',
+
     'slingsby.general.context_processors.default',
     'slingsby.quotes.context_processors.default',
+
     'social_auth.context_processors.social_auth_by_name_backends',
     'social_auth.context_processors.social_auth_backends',
     'social_auth.context_processors.social_auth_by_type_backends',
     'social_auth.context_processors.social_auth_login_redirect',
 )
-
-# Used for the query debugger that's run in dev mode.
-INTERNAL_IPS = ("127.0.0.1", "::1")
 
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates'),
@@ -107,26 +83,7 @@ TEMPLATE_DIRS = (
 
 ROOT_URLCONF = 'slingsby.urls'
 
-if DEBUG:
-    STATIC_URL = '/static/'
-else:
-    STATIC_URL = 'http://org.ntnu.no/telemark/static/'
-
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': lambda req: DEBUG,
-}
-
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
+STATIC_URL = 'http://org.ntnu.no/telemark/static/'
 
 # social_auth specific settings
 SOCIAL_AUTH_DEFAULT_USERNAME = 'new_social_auth_user'
