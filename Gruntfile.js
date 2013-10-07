6ac96816230316df6b2a26470ec1f574ac11848a
Tarjei Husøy <tarjei@roms.no>
2013-10-07 23:08:12 +0200

    Add grunt tasks clean, pybuild, provision and deploy.
    clean removes *.pyc files and build artifacts.
    
    pybuild builds the slingsby.tar.gz that can be pip installed on the
    server.
    
    provision installs everything necessary on the server.
    
    deploy pushes the slingsby.tar.gz to the server and installs it.

--- Gruntfile.js
+++ Gruntfile.js
@@ -111,23 +111,67 @@ module.exports = function (grunt) {
           rcfile: '.pylintrc',
           virtualenv: 'venv_slingsby',
         },
-        src: 'slingsby',
+        src: ['slingsby', 'local_settings.py'],
       }
-    }
+    },
 
+    shell: {
+      options: {
+        stderr: true,
+      },
+      build_python: {
+        command: 'python setup.py sdist --dist-dir build --formats gztar',
+      },
+      provision: {
+        options: {
+            stdout: true,
+        },
+        command: [
+            'tar czf build/salt_and_pillar.tar.gz salt pillar',
+            'scp build/salt_and_pillar.tar.gz slingsby:/tmp/',
+            'ssh slingsby "sudo tar xf /tmp/salt_and_pillar.tar.gz -C /srv/',
+                'sudo salt-call --local state.highstate --force-color"'
+        ].join('&&'),
+      },
+      deploy: {
+        options: {
+            stdout: true
+        },
+        command: [
+            'scp build/slingsby-1.0.0.tar.gz slingsby:/tmp/slingsby.tar.gz',
+            'ssh slingsby "sudo /srv/ntnuita.no/venv/bin/pip uninstall slingsby -y || echo',
+                'sudo /srv/ntnuita.no/venv/bin/pip install /tmp/slingsby.tar.gz',
+                'sudo restart uwsgi"'
+        ].join(' && '),
+      }
+    },
+
+    clean: {
+      python: [
+        'slingsby/**/*.pyc',
+      ],
+      builds: [
+        'build',
+        'slingsby.egg-info',
+      ]
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-handlebars');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-pylint');
+  grunt.loadNpmTasks('grunt-shell');
 
   // Default tasks
   grunt.registerTask('default', ['watch']);
   grunt.registerTask('lint', ['jshint', 'pylint']);
-  grunt.registerTask('build', ['handlebars', 'compass', 'copy:srcToStatic']);
-  grunt.registerTask('deploy', ['copy:main']);
+  grunt.registerTask('build', ['handlebars', 'compass', 'copy:srcToStatic', 'pybuild']);
+  grunt.registerTask('deploy', ['shell:deploy']);//, 'copy:main']);
+  grunt.registerTask('pybuild', ['clean:builds', 'shell:build_python']);
+  grunt.registerTask('provision', ['shell:provision']);
 
 };
