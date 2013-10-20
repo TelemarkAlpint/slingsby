c860db45238b35d755e5923537313f0e1ff81130
Tarjei Husøy <tarjei@roms.no>
2013-10-21 01:17:23 +0200

    Remove legacy breakout script.
    An heirloom from the ntnui.no days of html frames. Glory days!

--- slingsby/static-src/js/breakout.js
+++
@@ -1,8 +0,0 @@
-(function breakoutOfFrame() {
-    "use strict";
-
-    var requestData = document.location.search;
-    var subpage = document.location.pathname.substr(18);
-    var redirectUrl = 'http://www.ntnuita.no';
-    top.location.href = redirectUrl + subpage + requestData;
-}());
