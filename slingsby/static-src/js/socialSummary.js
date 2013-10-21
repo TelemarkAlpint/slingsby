00fa8e63d6644e19f37d2d4c4318fbb74184e760
Tarjei Husøy <tarjei@roms.no>
2013-10-22 00:17:28 +0200

    Make sure the social summary binds wait until document ready.

--- slingsby/static-src/js/socialSummary.js
+++ slingsby/static-src/js/socialSummary.js
@@ -1,11 +1,14 @@
 (function ($) {
     "use strict";
 
-    $(".social_summary_textbox").each(function () {
-        $(this).after('<br><span>Antall tegn: 0</span>');
-        $(this).keyup(function () {
-            var length = this.value.length;
-            $(this).siblings("span").html("Antall tegn: " + length);
+    $(document).ready(function () {
+        $(".social_summary_textbox").each(function () {
+            $(this).after('<br><span>Antall tegn: 0</span>');
+            $(this).keyup(function () {
+                var length = this.value.length;
+                $(this).siblings("span").html("Antall tegn: " + length);
+            });
         });
     });
+
 }(jQuery));
