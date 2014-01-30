4d7b8dc0e96da686b1da2194b9b6bf21a0fe4127
Tarjei Husøy <tarjei@roms.no>
2014-01-30 13:01:18 +0100

    Update django-debug-toolbar to latest version

--- dev_settings.py
+++ dev_settings.py
@@ -38,24 +38,15 @@ STATICFILES_DIRS = (
 )
 
 DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': lambda req: DEBUG,
+    'SHOW_TOOLBAR_CALLBACK': '%s.true' % __name__,
 }
 
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
+def true(_):
+    return True
 
-MIDDLEWARE_CLASSES = tuple(list(MIDDLEWARE_CLASSES) + [
+MIDDLEWARE_CLASSES = tuple([
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-])
+] + list(MIDDLEWARE_CLASSES))
 
 INSTALLED_APPS = tuple(list(INSTALLED_APPS) + [
     'debug_toolbar',
