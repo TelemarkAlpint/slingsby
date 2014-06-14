11e031c6bddc51a1123feb9e4cee2921ba382266
Tarjei Husøy <tarjei@roms.no>
2014-06-14 20:44:48 +0200

    Restart uwsgi on the end of deployment

--- fabfile.py
+++ fabfile.py
@@ -49,6 +49,7 @@ def deploy():
         sudo('find static -type d -print0 | xargs -0 chmod 755')
     run('rm /tmp/static_files.tar.gz')
     migrate_db()
+    sudo('service uwsgi restart')
 
 
 @hosts('vagrant@127.0.0.1:2222')
