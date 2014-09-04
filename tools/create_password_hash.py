4ffe6d9fc35a9174995b8bcdf4fcbf9a74ed0b73
Tarjei Husøy <git@thusoy.com>
2014-09-04 18:48:44 +0200

    Delint and add some comments to create_password_hash

--- tools/create_password_hash.py
+++ tools/create_password_hash.py
@@ -3,20 +3,29 @@
 # Get a password hash in crypt form
 # This script must be run in a *nix environment (the studorg server works)
 
+from __future__ import print_function
+
 import crypt
 import os
 import base64
 import getpass
 
+# How much we should slow down the hash computation. The higher the number of
+# rounds, the harder it is to brute force the actual passwords if the shadow
+# file is leaked.
 ROUNDS = 10000
 
-password = 1
-password_repeat = 2
-while password != password_repeat:
-    password = getpass.getpass('Enter password: ')
-    password_repeat = getpass.getpass('Repeat password: ')
-    if password != password_repeat:
+def main():
+    password = 1
+    password_repeat = 2
+    while password != password_repeat:
+        password = getpass.getpass('Enter password: ')
+        password_repeat = getpass.getpass('Repeat password: ')
+        if password != password_repeat:
             print('Passwords do not match, try again.')
 
-salt = base64.b64encode(os.urandom(6))
-print(crypt.crypt(password, "$6$rounds={rounds}${salt}".format(rounds=ROUNDS, salt=salt)))
+    salt = base64.b64encode(os.urandom(6))
+    print(crypt.crypt(password, "$6$rounds={rounds}${salt}".format(rounds=ROUNDS, salt=salt)))
+
+if __name__ == '__main__':
+    main()
