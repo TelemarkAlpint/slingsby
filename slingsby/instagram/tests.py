e244beb965623071aad25f9c57e0e8b399c4c0cd
Tarjei Husøy <git@thusoy.com>
2014-09-19 14:07:23 +0200

    Add test that instagram page loads correctly

--- slingsby/instagram/tests.py
+++ slingsby/instagram/tests.py
@@ -1,10 +1,11 @@
 from .models import InstagramMedia, InstagramComment
 from .tasks import fetch_instagram_media
 
-import os
+import datetime
 import json
+import os
 from mock import patch
-from django.test import TestCase
+from django.test import Client, TestCase
 
 class ResponseMock(object):
 
@@ -29,3 +30,25 @@ class InstagramTasksTest(TestCase):
             fetch_instagram_media()
         self.assertEqual(InstagramMedia.objects.count(), 20)
         self.assertEqual(InstagramComment.objects.count(), 33)
+
+class InstagramPageTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        InstagramMedia.objects.create(
+            media_type='image',
+            poster='ntnuitaadmin',
+            poster_image='/admin.png',
+            thumbnail_url='/thumb.jpg',
+            media_url='/fullsize.jpg',
+            like_count=1337,
+            caption='Snow and awesome stuff this weekend with #ntnuita',
+            created_time=datetime.datetime.now(),
+            instagram_id='cafed00d',
+        )
+
+
+    def test_instagram_page(self):
+        response = self.client.get('/instagram/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('Snow and awesome stuff' in response.content.decode('utf-8'))
