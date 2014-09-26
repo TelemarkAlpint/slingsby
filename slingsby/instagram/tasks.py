c55edadeab7ab8ccd506f255bdb96e96d6907513
Tarjei Husøy <git@thusoy.com>
2014-09-26 15:31:47 +0200

    Delint instagram tasks

--- slingsby/instagram/tasks.py
+++ slingsby/instagram/tasks.py
@@ -3,8 +3,8 @@
 from .models import InstagramMedia, InstagramComment
 
 import requests
-import datetime
 from celery import shared_task
+from datetime import datetime
 from django.conf import settings
 from functools import wraps
 from logging import getLogger
@@ -36,6 +36,7 @@ def fetch_instagram_media():
     recent_media = requests.get('https://api.instagram.com/v1/tags/ntnuita/media/recent',
         params={'client_id': settings.INSTAGRAM_CLIENT_ID}).json()
     for media in recent_media['data']:
+        media_key = 'videos' if media['type'] == 'video' else 'images'
         instagram_media, created = InstagramMedia.objects.get_or_create(
             instagram_id=media['id'],
             defaults={
@@ -43,22 +44,22 @@ def fetch_instagram_media():
                 'poster': media['user']['username'],
                 'poster_image': media['user']['profile_picture'],
                 'thumbnail_url': media['images']['thumbnail']['url'],
-                'media_url': media['videos' if media['type'] == 'video' else 'images']['standard_resolution']['url'],
+                'media_url': media[media_key]['standard_resolution']['url'],
                 'like_count': media['likes']['count'],
                 'caption': media['caption']['text'],
-                'created_time': datetime.datetime.utcfromtimestamp(float(media['created_time'])),
+                'created_time': datetime.utcfromtimestamp(float(media['created_time'])),
             }
         )
         if created:
             _logger.info('New instagram media added (%s): %s', instagram_media.instagram_id,
                 instagram_media.caption)
         for comment in media['comments']['data']:
-            InstagramComment.objects.get_or_create(
+            instagram_comment, comment_created = InstagramComment.objects.get_or_create(
                 instagram_id=comment['id'],
                 defaults={
                     'poster': comment['from']['username'],
                     'poster_image': comment['from']['profile_picture'],
-                    'created_time': datetime.datetime.utcfromtimestamp(float(comment['created_time'])),
+                    'created_time': datetime.utcfromtimestamp(float(comment['created_time'])),
                     'text': comment['text'],
                     'media': instagram_media,
                 }
