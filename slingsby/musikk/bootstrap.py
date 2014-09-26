03ff255ac6adbec898c786fed0c190405b48ecaf
Marte Fossland <marte_fossland@hotmail.com>
2014-09-26 11:37:43 +0200

    bootstrap more songs

--- slingsby/musikk/bootstrap.py
+++ slingsby/musikk/bootstrap.py
@@ -1,5 +1,8 @@
 from .models import Song
 
+from django.contrib.webdesign.lorem_ipsum import words
+import random
+
 def bootstrap():
     Song.objects.get_or_create(title='Danger Zone',
         artist='Kenny Loggins',
@@ -17,3 +20,12 @@ def bootstrap():
         votes=3,
         ready=True,
     )
+    for i in range(41):
+        Song.objects.get_or_create(
+            artist='Artist %d' % i,
+            ready=True,
+            defaults={
+                'popularity': random.randint(0, 100),
+                'title': ' '.join(random.sample(words(40).split(), random.randint(2, 5))).title(),
+            }
+        )
