ac3c9b9ee367bc0c826316f5711c76a353d18c02
Tarjei Husøy <git@thusoy.com>
2015-01-22 22:57:23 +0100

    Fix suspiciousfileoperation in archive tests

--- slingsby/archive/tests.py
+++ slingsby/archive/tests.py
@@ -1,12 +1,14 @@
 from .models import Event, EventForm, Image
-from ..general.utils import get_permission, disconnect_signal
+from ..general.utils import get_permission, disconnect_signal, ignored
 
 from django.test import Client, TestCase
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.files import File
 from django.db.models.signals import post_save
 from mock import patch
 import os.path
+import shutil
 
 
 class ArchiveEventUploadTest(TestCase):
@@ -71,9 +73,21 @@ class ArchiveFrontPageTest(TestCase):
     def setUp(self):
         testevent = Event.objects.create(name='Testevent', startdate='2014')
         test_image = os.path.join(os.path.dirname(__file__), 'test-data', '1-thumb.jpg')
+        self.image_in_media_root = os.path.join(settings.MEDIA_ROOT, 'test-image.jpg')
+
+        # Copy image into media root to prevent django from raising suspiciousfileoperation
+        shutil.copy2(test_image, self.image_in_media_root)
+
         with disconnect_signal(post_save, Image):
-            with open(test_image, 'rb') as img_fh:
-                Image.objects.create(original=File(img_fh), event=testevent, ready=True)
+            with open(self.image_in_media_root, 'rb') as img_fh:
+                self.image = Image.objects.create(original=File(img_fh), event=testevent, ready=True)
+
+
+    def tearDown(self):
+        with ignored(OSError):
+            os.remove(self.image_in_media_root)
+            #from nose.tools import set_trace as f; f()
+            os.remove(self.image.original.path)
 
 
     def test_archive_page(self):
@@ -89,7 +103,7 @@ class ArchiveFrontPageTest(TestCase):
 
 class ArchiveBaseTest(TestCase):
 
-    def test_valid_event_dates(self):
+    def test_invalid_event_dates(self):
         invalid_strings = (
             '1976',
             '20014',
@@ -112,6 +126,8 @@ class ArchiveBaseTest(TestCase):
             test_form = EventForm(form_data)
             self.assertFalse(test_form.is_valid())
 
+
+    def test_valid_event_dates(self):
         valid_strings = (
             '2014',
             '2013-01',
@@ -119,10 +135,10 @@ class ArchiveBaseTest(TestCase):
             '2014-10-12',
             '    2014',
         )
+        form_data = {'name': 'Event name'}
         for datestring in valid_strings:
             form_data.update({'startdate': datestring})
             test_form = EventForm(form_data)
-            print str(test_form.errors).decode('utf-8')
             self.assertTrue(test_form.is_valid())
 
 
