9f42481aff7dd2304fec0f04753ab89d7534d52c
Tarjei Husøy <git@thusoy.com>
2015-05-21 13:10:50 +0200

    Use dummy caching in dev

--- dev_settings.py
+++ dev_settings.py
@@ -36,7 +36,7 @@ DATABASES = {
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
         'LOCATION': 'slingsby_rel'
     }
 }
