c4b2617d10cbd639db02051a5b6aa3fcd58ce8cb
lysne <lysne@live.no>
2015-08-27 21:37:52 +0200

    Added admin Anders

--- slingsby/settings.py
+++ slingsby/settings.py
@@ -15,6 +15,7 @@ DEBUG = False
 ADMINS = (
     ('NTNUI TA Webmasters', 'telemark-webmaster@ntnui.no'),
     ('Tarjei Husøy', 'apps-telemarkwebmaster@thusoy.com'),
+    ('Anders Lysne', 'lysne@live.no')
 )
 
 USE_TZ = True
