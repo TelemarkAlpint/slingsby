e2e5d24dc4c3f9e5cf30a1238dd4afb1f9d13e21
Tarjei Husøy <git@thusoy.com>
2015-11-17 13:03:58 -0800

    Bootstrap alina devecerski for all songs

--- slingsby/musikk/bootstrap.py
+++ slingsby/musikk/bootstrap.py
@@ -9,16 +9,19 @@ import os
 def bootstrap():
     # Create some songs on the toplist
     Song.objects.get_or_create(title='Danger Zone',
+        filename="musikk/alina-devecerski/flytta",
         artist='Kenny Loggins',
         popularity=30,
         ready=True,
     )
     Song.objects.get_or_create(title='Radioactive',
+        filename="musikk/alina-devecerski/flytta",
         artist='Imagine Dragons',
         ready=True,
         votes=2,
     )
     Song.objects.get_or_create(title='Better Off Alone',
+        filename="musikk/alina-devecerski/flytta",
         artist='Alice Deejay',
         popularity=50,
         votes=3,
@@ -27,13 +30,14 @@ def bootstrap():
 
     # Put a converted song in external media for playback
     source_file = os.path.join(os.path.dirname(__file__), 'test-data', 'flytta.mp3')
-    music_dir = os.path.join(settings.MEDIA_ROOT, 'musikk')
+    music_dir = os.path.join(settings.MEDIA_ROOT, 'external', 'musikk')
     create_target_directory_and_copy(source_file, os.path.join(music_dir, 'compilations', 'latest.mp3'))
     create_target_directory_and_copy(source_file, os.path.join(music_dir, 'alina-devecerski', 'flytta.mp3'))
 
     # Create some random filler songs
     for i in range(41):
         Song.objects.get_or_create(
+            filename="musikk/alina-devecerski/flytta",
             artist='Artist %d' % i,
             ready=True,
             defaults={
@@ -41,7 +45,6 @@ def bootstrap():
                 'title': ' '.join(random.sample(words(40).split(), random.randint(2, 5))).title(),
             }
         )
-
 
     # Create some new song suggestions
     Song.objects.get_or_create(title='The Bumpi Song',
@@ -52,5 +55,5 @@ def bootstrap():
 def create_target_directory_and_copy(source, dest):
     target_dir = os.path.dirname(dest)
     if not os.path.exists(target_dir):
-        os.makekdirs(target_dir)
+        os.makedirs(target_dir)
     shutil.copy2(source, dest)
