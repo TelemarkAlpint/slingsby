1a13bfe9ed0288ff7f7402cabe1c9b7a976ef661
Anders Vik Lysne <Anders@Anderss-MacBook-Pro.local>
2017-05-15 20:08:36 +0200

    removing previous modifications

--- slingsby/musikk/views.py
+++ slingsby/musikk/views.py
@@ -94,7 +94,6 @@ class SongDetailView(ActionView, TemplateView):
             Vote.objects.create(user=request.user, song=context['song'])
             logging.info('%s voted on %s.', request.user, context['song'])
             count_votes.delay()
-            update_top_songs()
             return HttpResponse('Vote registered on %s.' % context['song'])
         else:
             logging.info('%s tried to vote more than once on %s.',
@@ -102,9 +101,6 @@ class SongDetailView(ActionView, TemplateView):
             return HttpResponse("Du har allerede stemt på denne sangen i dag!",
                 content_type='text/plain', status=403)
 
-
-def update_top_songs(self):
-    self.top_songs = Song.objects.filter(ready=True).filter(votes__gt=0)[:_SONGS_IN_TOP_SONGS]
 
 def get_votes_from_today(user):
     """ Find the ids of the songs the user has voted on today. """
